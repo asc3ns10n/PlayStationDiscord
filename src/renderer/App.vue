85166fa9e551d29a02bd60b3af84851a77500bdc
Tustin <Tustin@users.noreply.github.com>
2020-09-04 16:45:51 -0500

    vue scaffold

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'playstation-discord'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
